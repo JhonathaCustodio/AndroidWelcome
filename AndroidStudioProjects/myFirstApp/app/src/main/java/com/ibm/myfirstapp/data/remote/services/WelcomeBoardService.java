6aaa6d3220c3f0ec30ce9595a1c585d9c67bc1bc
JhonathaCustodio <custodiojhonatha@gmail.com>
2022-11-01 11:32:07 -0300

    REFACTOR: Recreating Heroku cloud server

--- AndroidStudioProjects/myFirstApp/app/src/main/java/com/ibm/myfirstapp/data/remote/services/WelcomeBoardService.java
+++ AndroidStudioProjects/myFirstApp/app/src/main/java/com/ibm/myfirstapp/data/remote/services/WelcomeBoardService.java
@@ -1,18 +1,18 @@
 package com.ibm.myfirstapp.data.remote.services;
 
-import com.ibm.myfirstapp.data.remote.Response;
+import com.ibm.myfirstapp.data.remote.UserResponse;
 import com.ibm.myfirstapp.data.remote.requests.Request;
 import com.ibm.myfirstapp.data.remote.requests.LoginUser;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
-
+S
 public interface WelcomeBoardService {
 
-        @POST("welcome/")
-        Call<Response> saveUser(@Body Request request);
+        @POST("welcome/register/")
+        Call<UserResponse> saveUser(@Body Request request);
 
-        @POST("welcome/")
-        Call<Response> loginUser(@Body LoginUser login);
+        @POST("welcome/login/")
+        Call<UserResponse> loginUser(@Body LoginUser login);
 }
